f08a691dde87bbc7fa2403e72a0115e31c3a1726
dikshagharde <117832798+dikshagharde@users.noreply.github.com>
2023-04-17 14:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+let team = players;
+let team1 = [].concat(players);
+
+let cap1 = Object.create(person);
+export {players,person,team,team1,cap1}
+
+
